ccc09cd1e31d2bc0609171f8f6bd82bb5714686a
Abhay Jatin Doshi <abhaydoshi@microsoft.com>
2025-08-07 00:50:31 +0530

    basic windows and android setup

---
+++ apps/desktop/windows/windows/SafeSpend/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::SafeSpend::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::SafeSpend::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
